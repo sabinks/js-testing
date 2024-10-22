29237323cae2cd4059aa040024ef7e84ff2297d1
sabinks <sabin.kr.stha@gmail.com>
2024-10-22 14:14:48 -0400

    class method is js

--- src/test/class.js
+++ src/test/class.js
@@ -1,14 +1,19 @@
 class Student {
-  constructor(fName, lName, year) {
+  constructor(fName, lName, year, late) {
     this.fName = fName;
     this.lName = lName;
     this.grade = year;
+    this.late = late;
   }
   fullName() {
     return `Your full name is ${this.fName} ${this.lName}`;
   }
+  markLate() {
+    this.late += 1;
+    return `${this.fName} ${this.lName} has been late ${this.late}`;
+  }
 }
 
-const saksham = new Student('Saksham', 'Shrestha', 'JK');
+const saksham = new Student('Saksham', 'Shrestha', 'JK', 1);
 console.log(saksham);
 console.log(saksham.fullName());
